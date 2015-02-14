b2fc6c75d9c0c64cfb7d4cb4f77ce14edab2aacb
Michał Bień <michal99999@gmail.com>
2015-02-14 12:45:02 +0100

    Small changes & licensing

--- rezunpacker.cpp
+++ rezunpacker.cpp
@@ -1,12 +1,44 @@
+/*  
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+    Niniejszy program jest wolnym oprogramowaniem; możesz go
+    rozprowadzać dalej i/lub modyfikować na warunkach Powszechnej
+    Licencji Publicznej GNU, wydanej przez Fundację Wolnego
+    Oprogramowania - według wersji 2 tej Licencji lub (według twojego
+    wyboru) którejś z późniejszych wersji.
+
+    Niniejszy program rozpowszechniany jest z nadzieją, iż będzie on
+    użyteczny - jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej
+    gwarancji PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH
+    ZASTOSOWAŃ. W celu uzyskania bliższych informacji sięgnij do
+    Powszechnej Licencji Publicznej GNU.
+
+    Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
+    Powszechnej Licencji Publicznej GNU (GNU General Public License);
+    jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
+    Place, Fifth Floor, Boston, MA  02110-1301  USA   
+*/
+
+
 #include <fstream>
 #include <string>
 #include <iostream>
-#include <sstream>
 #include <vector>
 #include <stdio.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <dirent.h>
 #include <unistd.h>
 
 void parseEntry(int offset, int maxoffset, std::string workdir);
